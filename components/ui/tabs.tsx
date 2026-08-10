3377b655701179e6e2d52fb23c7eef71e4d420da
MasterSaptak <burningsoulofdarkness@gmail.com>
2026-08-10 14:52:39 +0530

    Clean and Clear

--- components/ui/tabs.tsx
+++
@@ -1,5 +0,0 @@
-import * as React from "react"
-export const Tabs = (props: React.HTMLAttributes<HTMLDivElement>) => <div {...props} />
-export const TabsList = (props: React.HTMLAttributes<HTMLDivElement>) => <div {...props} />
-export const TabsTrigger = (props: React.HTMLAttributes<HTMLDivElement>) => <div {...props} />
-export const TabsContent = (props: React.HTMLAttributes<HTMLDivElement>) => <div {...props} />
